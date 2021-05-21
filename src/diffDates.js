64bf05c9d3bc03079d2fca4e432177d5555016ee
Павел Андреевич <71135153+PavelIlyin@users.noreply.github.com>
2021-05-22 00:56:44 +1000

    lesson-2

---
+++ src/diffDates.js
@@ -0,0 +1,43 @@
+import { DateTime }  from 'luxon';
+
+
+export function diffDates(firstDate, secondDate) {
+    firstDate = DateTime.fromISO(firstDate);
+    secondDate = DateTime.fromISO(secondDate);
+
+    if (firstDate > secondDate) {
+        [firstDate, secondDate] = [secondDate, firstDate];
+    }
+
+    return secondDate.diff(firstDate, ['years', 'months', 'days']).toObject();
+}
+
+export const diffToHTML = diff => `
+    <span>
+        Лет: ${diff.years},
+        Месяцев: ${diff.months},
+        Дней: ${diff.days}        
+    </span>`
+
+export function time(hour, min, sec) {
+
+    if (sec != 0) {
+        sec = sec - 1;
+        sec = sec < 10 ? '0' + sec : sec;
+    } else {
+        if (min != 0) {
+            min = min - 1;
+            sec = 59;
+            min = min < 10 ? '0' + min : min;
+        } else {
+            if (hour != 0) {
+                hour = hour - 1;
+                min = 59;
+                sec = 59;
+                hour = hour < 10 ? '0' + hour : hour;
+            }
+        }
+    }
+    
+    return [hour, min, sec];
+}
